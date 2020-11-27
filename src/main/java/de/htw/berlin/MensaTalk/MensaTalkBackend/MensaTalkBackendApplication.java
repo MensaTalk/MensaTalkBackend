9174079841a06a7f87ec02d2db99a18b66b31122
OliKue# <oliver-kuetemeier123@web.de>
2020-11-27 16:32:30 +0100

    init Spring Project

---
+++ src/main/java/de/htw/berlin/MensaTalk/MensaTalkBackend/MensaTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.MensaTalk.MensaTalkBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MensaTalkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MensaTalkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/htw/berlin/MensaTalk/MensaTalkBackend/MensaTalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.htw.berlin.MensaTalk.MensaTalkBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MensaTalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
