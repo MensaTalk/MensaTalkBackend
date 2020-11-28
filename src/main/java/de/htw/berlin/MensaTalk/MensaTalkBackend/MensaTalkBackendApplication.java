2e25e2bab61716097f0f269eeeca3e76b9c74e90
OliKue# <oliver-kuetemeier123@web.de>
2020-11-28 13:10:52 +0100

    add testData

--- src/main/java/de/htw/berlin/MensaTalk/MensaTalkBackend/MensaTalkBackendApplication.java
+++ src/main/java/de/htw/berlin/MensaTalk/MensaTalkBackend/MensaTalkBackendApplication.java
@@ -1,7 +1,11 @@
 package de.htw.berlin.MensaTalk.MensaTalkBackend;
 
+import de.htw.berlin.MensaTalk.MensaTalkBackend.ChatRoom.ChatRoom;
+import de.htw.berlin.MensaTalk.MensaTalkBackend.ChatRoom.ChatRoomRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MensaTalkBackendApplication {
@@ -10,4 +14,11 @@ public class MensaTalkBackendApplication {
 		SpringApplication.run(MensaTalkBackendApplication.class, args);
 	}
 
+	@Bean
+	ApplicationRunner applicationRunner(ChatRoomRepository chatRoomRepository){
+		return args -> {
+			chatRoomRepository.save(new ChatRoom(1l, "testRoom1",null,null));
+			chatRoomRepository.save(new ChatRoom(1l, "testRoom2",null,null));
+		};
+	}
 }
