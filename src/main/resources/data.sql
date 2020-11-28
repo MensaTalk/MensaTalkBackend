48335ac1ab421234cdf629fdd8f478e31d1c24c5
OliKue# <oliver-kuetemeier123@web.de>
2020-11-28 16:54:30 +0100

    update data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -4,15 +4,15 @@ INSERT INTO chat_room (chat_room_id, name) VALUES ('3', 'Room 3');
 INSERT INTO chat_room (chat_room_id, name) VALUES ('4', 'Room 4');
 
 
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('1', 'first message room 1','1');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('2', 'second message room 1','1');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('3', 'thid message room 1','1');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('4', 'first message room 2','2');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('5', 'second message room 2','2');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('6', 'third message room 2','2');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('7', 'first message room 3','3');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('8', 'second message room 3','3');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('9', 'third message room 3','3');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('10', 'first message room 4','4');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('11', 'second message room 4','4');
-INSERT INTO chat_message (chat_message_id, text_message, chat_room_id) VALUES ('12', 'third message room 4','4');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('1', current_timestamp ,'first message room 1','1');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('2', current_timestamp , 'second message room 1','1');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('3', current_timestamp , 'thid message room 1','1');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('4', current_timestamp , 'first message room 2','2');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('5', current_timestamp , 'second message room 2','2');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('6', current_timestamp , 'third message room 2','2');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('7', current_timestamp , 'first message room 3','3');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('8', current_timestamp , 'second message room 3','3');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('9', current_timestamp , 'third message room 3','3');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('10', current_timestamp , 'first message room 4','4');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('11', current_timestamp , 'second message room 4','4');
+INSERT INTO chat_message (chat_message_id, created_at, text_message, chat_room_id) VALUES ('12', current_timestamp , 'third message room 4','4');
