a2f75eaea667db4af70d2df7bd2475c7344db9a1
OliKue# <oliver-kuetemeier123@web.de>
2020-11-28 17:00:24 +0100

    change datainitialization

--- src/main/java/de/htw/berlin/MensaTalk/MensaTalkBackend/MensaTalkBackendApplication.java
+++ src/main/java/de/htw/berlin/MensaTalk/MensaTalkBackend/MensaTalkBackendApplication.java
@@ -15,17 +15,5 @@ public class MensaTalkBackendApplication {
         SpringApplication.run(MensaTalkBackendApplication.class, args);
     }
 
-    @Bean
-    ApplicationRunner applicationRunner(ChatRoomRepository chatRoomRepository, ChatMessageRepository chatMessageRepository) {
-        return args -> {
-            //Create 3 Rooms
-            chatRoomRepository.save(new ChatRoom(1l, "testRoom1", null));
-            chatRoomRepository.save(new ChatRoom(2l, "testRoom2", null));
-            chatRoomRepository.save(new ChatRoom(3l, "testRoom1", null));
 
-
-
-
-        };
-    }
 }
