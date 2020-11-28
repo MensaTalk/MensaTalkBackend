a0d0bafad336b6aa8a4d6708b018c840884552dc
OliKue# <oliver-kuetemeier123@web.de>
2020-11-28 14:31:59 +0100

    fix findbyChatRoom Method

--- src/main/java/de/htw/berlin/MensaTalk/MensaTalkBackend/ChatMessage/ChatMessageRepository.java
+++ src/main/java/de/htw/berlin/MensaTalk/MensaTalkBackend/ChatMessage/ChatMessageRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
-    List<ChatMessage> findAllByRoom(ChatRoom chatRoom);
+    List<ChatMessage> findAllByChatRoom(ChatRoom chatRoom);
 }
