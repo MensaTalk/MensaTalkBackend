4c9deee335c0df6b57bd12a7543566696917afaa
OliKue# <oliver-kuetemeier123@web.de>
2020-11-29 13:09:06 +0100

    add specific origin to websocket

--- src/main/java/de/htw/berlin/MensaTalkBackend/WebSocketServer/configuration/WebSocketConfiguration.java
+++ src/main/java/de/htw/berlin/MensaTalkBackend/WebSocketServer/configuration/WebSocketConfiguration.java
@@ -13,7 +13,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/chatsocket").setAllowedOrigins("*").withSockJS();
+        registry.addEndpoint("/chatsocket").setAllowedOrigins("chrome-extension://*").withSockJS();
     }
 
     @Override
