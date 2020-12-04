b48ed38c088d08dfbe40043881b940ff1d15214a
OliKue# <oliver-kuetemeier123@web.de>
2020-12-04 10:11:28 +0100

    add jwttutorial

---
+++ src/main/java/de/htw/berlin/jwt/db/UserRepository.java
@@ -0,0 +1,11 @@
+package de.htw.berlin.jwt.db;
+
+import de.htw.berlin.jwt.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
